aa433e9017ce797dc44b937c42f60a046b2dc87c
txv.dev <txv.dev@gmail.com>
2022-11-27 19:48:16 +0100

    Component configuration porPais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
-  constructor() { }
+  termino: string = "";
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor( private PaisService: PaisService ) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(){
+    this.hayError = false;
+    console.log(this.termino);
+
+    // this.PaisService.buscarPais( this.termino )
+    //   .subscribe( (paises )=> {
+    //     console.log(paises)
+    //   }, (err) => {
+    //     this.hayError = true;
+    //   },)
+    // this.PaisService.buscarPais( this.termino )
+    //   .subscribe(
+    //     paises => console.log(paises),
+    //     err => this.hayError = true,
+    //     () => console.log('un exito')
+    //    );
+
+    this.PaisService.buscarPais( this.termino )
+      .subscribe({
+        next: (paises) => { console.log(paises);
+                          this.paises = paises;
+                        },
+        error: () => { this.hayError = true;
+                        this.paises = [];
+                      },
+        complete: () => { console.log('Tarea completa') }
+      })
+
+    // this.termino = '';
   }
 
 }
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -21,7 +24,9 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     VerPaisComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule
   ]
 })
 export class PaisModule { }
